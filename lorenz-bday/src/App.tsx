21ea0324c5771c1a30972cd2e77309cf5b51fa61
Olivia Laurel <OLaurel113@gmail.com>
2025-07-16 20:56:37 -0500

    picture headers colors

--- lorenz-bday/src/App.tsx
+++ lorenz-bday/src/App.tsx
@@ -1,11 +1,50 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const images = [
+    '/images/bday.jpeg',
+    '/images/din.jpeg',
+    '/images/handy.jpeg',
+    '/images/nom.jpeg',
+    '/images/cap.jpeg',
+    '/images/makeup.jpeg',
+    '/images/pt.jpeg',
+    '/images/sha.jpeg',
+    '/images/flow.jpeg',
+    '/images/seum.jpeg',
+    '/images/dinner.jpeg',
+    '/images/sleep.jpeg',
+    '/images/cmas.jpeg',
+    '/images/kissy.jpeg',
+    '/images/npm.jpeg',
+   
+  ]; 
+
   return (
-    <div className="bg-[#fffbf2] min-h-screen">
-      <div className="flex justify-center items-center fixed top-10 w-full bg-[#fffbf2] py-4">
-        <h1 className="text-black text-4xl font-bold">Happy 22 Lorenz!</h1>
+    <div className="flex min-h-screen">
+      <div className="bg-[#97d0ff] w-2/5"></div>
+
+      <div className="flex flex-col justify-start items-start w-3/5 p-10">
+        <h1 className="text-black text-5xl font-bold mb-4">
+          Happy 22 Lorenz!
+        </h1>
+        <p className="text-lg text-gray-700 mb-6">hi cutie!</p>
+        
+        <div className="overflow-hidden w-full">
+          <div className="flex scroll">
+            {images.concat(images).map((src, index) => (
+              <img
+                key={index}
+                src={src}
+                alt={`Gallery Image ${index + 1}`}
+                className="w-48 h-48 object-cover mx-2"
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
