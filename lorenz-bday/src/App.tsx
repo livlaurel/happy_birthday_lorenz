d01d14eace7b6878ec8f9d0b47766227402ade8a
Olivia Laurel <OLaurel113@gmail.com>
2025-07-16 21:53:23 -0500

    more design

--- lorenz-bday/src/App.tsx
+++ lorenz-bday/src/App.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from 'react';
-
 function App() {
   const images = [
     '/images/bday.jpeg',
@@ -17,27 +15,33 @@ function App() {
     '/images/cmas.jpeg',
     '/images/kissy.jpeg',
     '/images/npm.jpeg',
-   
   ]; 
 
   return (
-    <div className="flex min-h-screen">
-      <div className="bg-[#97d0ff] w-2/5"></div>
+    <div className="flex min-h-screen bg-[#fffcf7]">
+      <div className="bg-[#97d0ff] w-2/5 flex justify-center items-center">
+        <img src="/images/cmas.jpeg" className="w-[90%] h-auto border-4 border-[#fffcf7] rounded-xl shadow-lg" />
+      </div>
 
       <div className="flex flex-col justify-start items-start w-3/5 p-10">
-        <h1 className="text-black text-5xl font-bold mb-4">
+        <h1 className="text-[#97d0ff] text-5xl font-bold mb-4">
           Happy 22 Lorenz!
         </h1>
-        <p className="text-lg text-gray-700 mb-6">hi cutie!</p>
+        <p className="text-lg text-gray-700 pt-6 mb-6">
+          hi cutie! &lt;3 <br /> <br />
+          i'm so excited we get to be together on your birthday!<br />
+          i am so in love with you and i hope you enjoy all your gifts!<br />
+          we have a lot planned for the weekend so get ready for the best birthday ever!
+        </p>
         
-        <div className="overflow-hidden w-full">
+        <div className="mt-auto w-full overflow-x-auto bg-[#f28a88]">
           <div className="flex scroll">
             {images.concat(images).map((src, index) => (
               <img
                 key={index}
                 src={src}
                 alt={`Gallery Image ${index + 1}`}
-                className="w-48 h-48 object-cover mx-2"
+                className="w-48 h-48 object-cover mx-2 border-t-8 border-b-8 border-l-4 border-[#f28a88]"
               />
             ))}
           </div>
